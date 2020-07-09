86c70fc0cb5ba3cfbacc0c3c34c299a448ad5ffb
Ivan Zou <izou3@gatech.edu>
2020-07-08 22:52:07 -0400

    Production Build

--- src/server.js
+++ src/server.js
@@ -2,14 +2,19 @@ import express from 'express';
 import { render } from '@jaredpalmer/after';
 import routes from './setup/routes';
 import Document from './Modules/common/Document';
+import path from 'path'; 
+
+const publicFolder = process.env.NODE_ENV === 'production' ? path.join(__dirname, '../build/public') : 'public'; 
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 const chunks = require(process.env.RAZZLE_CHUNKS_MANIFEST);
 
+//process.env.RAZZLE_PUBLIC_DIR 
+
 const server = express();
 server
   .disable('x-powered-by')
-  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
+  .use(express.static(publicFolder))
   .get('/*', async (req, res) => {
     try {
       const html = await render({
